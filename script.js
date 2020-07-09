9d4ed7c4ed006f1909a94d1bc917c3f34775601b
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2020-07-09 14:41:59 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Due date
+let expiryDate = new Date("Jul 9, 2020 14:40:00").getTime();
+
+
+let x = setInterval(() => {
+    
+    // Get curreent date and time
+    let present = new Date().getTime();
+    
+    let duration = expiryDate - present;
+    
+  let days = Math.floor(duration / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((duration % (1000 * 60)) / 1000);
+    
+
+  document.querySelector(".day").innerHTML = days + 'd'; 
+  document.querySelector(".hour").innerHTML = hours + 'h';
+  document.querySelector(".mins").innerHTML = minutes + 'm';
+  document.querySelector(".sec").innerHTML = seconds + 's';
+
+    if (duration < 0) {
+    clearInterval(x);
+    let box = document.querySelector(".box");
+    let h = document.getElementsByTagName("h5");
+    box.style.display= 'none';
+    h.style.display= 'none';
+    document.querySelector(".show").innerHTML = "It's Sunday";
+  }
+}, 1000);
